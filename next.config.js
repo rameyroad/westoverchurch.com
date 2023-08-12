76b22cc70505db2ded10e219e25cdf9b1a9a0a69
Jason Moore <mooreja@ccl.org>
2023-08-11 20:50:58 -0400

    Stand alone build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
 
-module.exports = nextConfig
+module.exports = (phase) => {
+    // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environment variable
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+    // when `next build` or `npm run build` is used
+    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+    // when `next build` or `npm run build` is used
+    const isStaging =
+        phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+    console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
+
+    const env = {};
+
+    // next.config.js object
+    return {
+        output: 'standalone',
+        env,
+    }
+}
