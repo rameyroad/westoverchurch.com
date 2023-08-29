2a320702dde62b8c762ef0b2fa053b09e2764946
Jason Moore <jason@jmit.us>
2023-08-29 15:00:43 -0400

    Favicon & description text

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,16 +10,22 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="utf-8" />
-                    <link rel="shortcut icon" href="https://assets.maccarianagency.com/favicons/thefront/favicon.ico" />
+                    <link rel="shortcut icon" href="/favicon/favicon.ico" />
                     <meta name="theme-color" content="#ffffff" />
-                    <meta name="description" content="A modern design system for your new landing and web pages." />
+                    <meta
+                        name="description"
+                        content="Westover Church, a non-denominational Church in Greensboro, North Carolina.  We strive to develop Maturing Followers of Jesus Christ."
+                    />
                     <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
                     <meta property="og:locale" content="en_US" />
                     <meta property="og:type" content="website" />
-                    <meta property="og:image" content="https://assets.maccarianagency.com/screenshots/the-front/social.png" />
+                    <meta property="og:image" content="/favicon/apple-touch-icon.png" />
                     <meta property="og:title" content="Westover Church" />
-                    <meta property="og:description" content="A modern design system for your new landing and web pages." />
-                    <meta property="og:url" content="https://thefront.maccarianagency.com/" />
+                    <meta
+                        property="og:description"
+                        content="Westover Church, a non-denominational Church in Greensboro, North Carolina.  We strive to develop Maturing Followers of Jesus Christ."
+                    />
+                    <meta property="og:url" content="https://www.westoverchurch.com/" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
                 </Head>
