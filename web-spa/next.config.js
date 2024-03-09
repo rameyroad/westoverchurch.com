39999aad059f07c1943bf45620342b081508ed73
Jason Moore <jason@jmit.us>
2024-03-09 16:49:03 -0500

    Files for new build

--- web-spa/next.config.js
+++ web-spa/next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     appDir: true,
   },
   images: {
     domains: ['assets.maccarianagency.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'westovercontent.blob.core.windows.net',
+        port: '',
+        pathname: '/cmscontent/**',
+      },
+    ],
   },
-}
- 
-module.exports = nextConfig
+};
 
-
-// // next.config.js
-// module.exports = {
-//   images: {
-//     domains: ['assets.maccarianagency.com'],
-//   },
-// }
+module.exports = nextConfig;
