7343e0fe4f9108077ed85fa7f78d51045d7d927d
Jason Moore <mooreja@ccl.org>
2024-05-14 16:28:30 -0400

    Fix build error

--- web-spa/src/app/page.tsx
+++ web-spa/src/app/page.tsx
@@ -4,13 +4,12 @@ import React, { useEffect } from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import IndexView from 'views/IndexView';
+// import IndexView from 'views/IndexView';
 
 const IndexPage = (): JSX.Element => {
   const router = useRouter();
 
   useEffect(() => {
-    console.log('IndexPage');
     router.push('/home');
   }, []);
 
